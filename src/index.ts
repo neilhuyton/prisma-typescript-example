bf4870d93dd888788003303dfa24afa9adbe7cb2
Neil Huyton <github@nehu.me>
2025-08-12 15:12:15 +0100

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
-import express, { Request, Response } from "express";
-import userRoutes from "./routes/user";
+import express, { Request, Response } from 'express';
+import serverless from 'serverless-http';
+import userRoutes from './routes/user';
 
 const app = express();
 app.use(express.json());
 
 // Routes
-app.use("/users", userRoutes);
+app.use('/users', userRoutes);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Simple Prisma + TypeScript API");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Simple Prisma + TypeScript API');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Export for Netlify Functions
+export const handler = serverless(app);
